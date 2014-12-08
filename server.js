0fee70ef2e8b93baa55d46d93d2c23cd5af455fb
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2014-12-08 16:39:40 +0100

    Locations form DB

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var http = require("http"),
-mongojs = require("mongojs");
+	mongojs = require("mongojs");
 
-var uri = "mongodb://demo_user:demo_password@ds027769.mongolab.com:27769/demo_database",
-	db = mongojs.connect(uri, ["demo_collection"]);
+var db = mongojs.connect("mongodb://localhost:27017/dota", ["locations", "distances", "zones"]);
 
 var server = http.createServer(requestHandler);
 
 function requestHandler(request, response) {
 	response.writeHead(200, {"Content-Type": "text/html"});
-	db.demo_collection.find({"color": "red"}, function(err, records) {
+	
+	db.locations.distinct('match', function(err,records) {
 		if(err) {
 			console.log("There was an error executing the database query.");
 			response.end();
@@ -18,12 +18,7 @@ function requestHandler(request, response) {
 		i = records.length;
 
 		while(i--) {
-			html += '<p><b>Name:</b> ' 
-			+ records[i].name 
-			+ ' <br /><b>Number of wheels:</b> ' 
-			+ records[i].wheels 
-			+ '<br /><b>Color: </b>' 
-			+ records[i].color;
+			html += '<p>'+records[i]+'</p>';
 		}
 		response.write(html);
 		response.end();
