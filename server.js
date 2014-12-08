186c129661a0e2169105588417b84c1d73255532
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2014-12-08 17:01:03 +0100

    First working example

--- server.js
+++ server.js
@@ -1,28 +1,20 @@
-var http = require("http"),
-	mongojs = require("mongojs");
+var http 	= require("http"),
+	mongojs = require("mongojs"),
+	Router 	= require('node-simple-router'),
+	router 	= new Router(),
+	db 		= mongojs.connect("mongodb://localhost:27017/dota", ["locations", "distances", "zones"]),
+	server 	= http.createServer(router);
 
-var db = mongojs.connect("mongodb://localhost:27017/dota", ["locations", "distances", "zones"]);
+router.get("/matches", function(request, response) {
 
-var server = http.createServer(requestHandler);
+	db.locations.distinct('match', function(err, records) {
+		if(err) response.writeHead(500);
 
-function requestHandler(request, response) {
-	response.writeHead(200, {"Content-Type": "text/html"});
-	
-	db.locations.distinct('match', function(err,records) {
-		if(err) {
-			console.log("There was an error executing the database query.");
-			response.end();
-			return;
-		}
-		var html = '<h2>Vehicles with a red finish</h2>',
-		i = records.length;
-
-		while(i--) {
-			html += '<p>'+records[i]+'</p>';
-		}
-		response.write(html);
+		response.writeHead(200, {"Content-Type": "text/json"});
+		response.write(JSON.stringify(records));
 		response.end();
-	})
-}
-		
+	});
+
+});
+
 server.listen(8888);
