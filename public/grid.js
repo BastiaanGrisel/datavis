36257a76d29bad99beae849946dc539b750d07a8
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2014-12-09 16:03:24 +0100

    Static resources on server

---
+++ public/grid.js
@@ -0,0 +1,57 @@
+var size = [128,128],
+	canvas_width  = document.getElementById("svg_container").clientWidth,
+	canvas_height = document.getElementById("svg_container").clientHeight,
+	grid_width    = canvas_width < canvas_height ? canvas_width : canvas_height,
+	grid_height   = grid_width;
+
+// Create a D3 container
+var svg = d3.select("#svg_container").append("svg")
+    .attr("width", canvas_width)
+    .attr("height", canvas_height)
+
+// Create a square container
+var grid = svg.append("g")
+    .attr("class", "grid")
+    .attr("width", grid_width)
+    .attr("height", grid_height)
+    // Position the grid to the center of the page
+    .attr("transform","translate("+ Math.floor( (canvas_width - grid_width)/2 ) +","+ Math.floor( (canvas_height - grid_height)/2 ) +")")
+
+// Define domain and range for the position of the tiles
+var x = d3.scale.ordinal()
+    .domain(d3.range(size[0]))
+    .rangeBands([0,grid_width]);
+
+var y = d3.scale.ordinal()
+    .domain(d3.range(size[1]))
+    .rangeBands([0,grid_height]);
+
+// Display data
+var color = d3.scale.category20();
+
+function updateGrid(tiles) {
+    grid.selectAll("rect")
+    .data(tiles)
+    .enter()
+    .append("rect")
+    .attr("width", function(d) {
+        return x.rangeBand();
+    })
+    .attr("height", function(d) {
+        return y.rangeBand();
+    })
+    .attr("x", function(d) {
+        return x(d.col);
+    })
+    .attr("y", function(d) {
+        return y(d.row);
+    })
+    .attr("fill", function(d,i) {
+        return color(i);
+    })
+}
+
+function Tile(row, col) {
+	this.row = row;
+	this.col = col;
+}
--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 var http 	= require("http"),
 	mongojs = require("mongojs"),
-	Router 	= require('node-simple-router'),
+	Router 	= require("node-simple-router"),
 	router 	= new Router(),
+	swig 	= require("swig"),
 	db 		= mongojs.connect("mongodb://localhost:27017/dota", ["locations", "distances", "zones"]),
 	server 	= http.createServer(router);
+
+// Serve grid 
+router.get("/grid", function(request, response) {
+	response.writeHead(200, {"Content-Type": "text/html"});
+
+	var html = swig.renderFile("grid.html");
+
+	response.write(html);
+	response.end();
+});
 
 router.get("/matches", function(request, response) {
 
 	db.locations.distinct('match', function(err, records) {
 		if(err) response.writeHead(500);
 
-		response.writeHead(200, {"Content-Type": "text/json"});
+		response.writeHead(200, {"Content-Type": "text/json", "Access-Control-Allow-Origin": "*"});
 		response.write(JSON.stringify(records));
 		response.end();
 	});
